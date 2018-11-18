3462be5829e99a61baf9009e198d290fdb40e26a
Sam Carty <samcarty311@gmail.com>
2018-11-18 15:37:13 +0000

    removed env

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\samca\onedrive\docume~1\progra~1\webdev~1\organi~1\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
