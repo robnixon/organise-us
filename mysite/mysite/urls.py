280a3c40d4fcd946dd8caf66f6b031339951caf4
Sam Carty <samcarty311@gmail.com>
2018-11-30 16:46:25 +0000

    updated webpage styling

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('social.urls')),
     path('polls/', include('polls.urls')),
-    path('social/', include('social.urls')),
     path('admin/', admin.site.urls),
 ]
--- mysite/social/models.py
+++ mysite/social/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 class Post(models.Model):
     post_text = models.CharField(max_length=2000)
     user = models.ForeignKey(User, unique=False, null=True, on_delete=models.SET_NULL)
-    pub_date = models.DateTimeField('date published', default=timezone.now(), editable=False)
+    pub_date = models.DateTimeField('date published', default=timezone.now, editable=False)
 
     def get_absolute_url(self):
         return reverse('social:detail', kwargs={'pk': self.pk})
