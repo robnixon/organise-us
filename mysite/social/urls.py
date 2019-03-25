390b9bb75e459a6538aa2db22715b06140a25ffc
Sam Carty <samcarty311@gmail.com>
2019-03-25 17:12:36 +0000

    rename about page

--- mysite/social/urls.py
+++ mysite/social/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('new/', views.PostCreate.as_view(), name="new_post"),
-    path('about', TemplateView.as_view(template_name='social/aboutUs.html'), name='about'),
+    path('about', TemplateView.as_view(template_name='social/about.html'), name='about'),
     url(r'^accounts/login/$', LoginView.as_view(template_name='social/login.html'), name='login'),
     url(r'^accounts/logout/$', LogoutView.as_view(template_name='social/logout.html'), name='logout'),
 ]
