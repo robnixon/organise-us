cdbd83e16a4efefcb4497a175712661e9e083a8b
Sam Carty <samcarty311@gmail.com>
2019-03-25 17:26:36 +0000

    bug fixes

--- mysite/social/urls.py
+++ mysite/social/urls.py
@@ -8,8 +8,8 @@ from . import views
 app_name = 'social'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('new/', views.PostCreate.as_view(), name="new_post"),
+    path('post/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('posts/new/', views.PostCreate.as_view(), name="new_post"),
     path('about', TemplateView.as_view(template_name='social/about.html'), name='about'),
     url(r'^accounts/login/$', LoginView.as_view(template_name='social/login.html'), name='login'),
     url(r'^accounts/logout/$', LogoutView.as_view(template_name='social/logout.html'), name='logout'),
